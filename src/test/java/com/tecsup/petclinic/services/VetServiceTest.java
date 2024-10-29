20dd0a3df305e4cb1ace2bc002b9a3a8aef92b2e
RosaLopezC <rosalopezxd@gmail.com>
2024-10-29 14:36:59 -0500

    Editado por Rosa Lopez(Create)

--- src/test/java/com/tecsup/petclinic/services/VetServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/VetServiceTest.java
@@ -1,11 +1,39 @@
 package com.tecsup.petclinic.services;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tecsup.petclinic.entities.Vet;
 
 @SpringBootTest
 @Slf4j
+@Transactional
 public class VetServiceTest {
 
-	
+    @Autowired
+    private VetService vetService;
+
+    @Test
+    public void testCreateVet() {
+        Vet vet = new Vet();
+        vet.setNombre("Carlos");
+        vet.setApellido("Gonzalez");
+        vet.setNumeroLicencia("LIC12345");
+        vet.setEspecialidad("Cirugía");
+
+        Vet savedVet = vetService.save(vet);
+
+        assertNotNull(savedVet.getId());
+        assertEquals("Carlos", savedVet.getNombre());
+        assertEquals("Gonzalez", savedVet.getApellido());
+        assertEquals("LIC12345", savedVet.getNumeroLicencia());
+        assertEquals("Cirugía", savedVet.getEspecialidad());
+
+        log.info("Test de creación de veterinario pasado correctamente.");
+    }
 }
